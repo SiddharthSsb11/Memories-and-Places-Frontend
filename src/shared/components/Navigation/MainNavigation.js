9d523a006e7f800473e6b2c13fa44cbbd398de3d
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2021-11-25 03:18:24 +0530

    Header UI Changed

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -34,7 +34,7 @@ const MainNavigation = (props) => {
           <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">YourPlaces</Link>
+          <Link to="/">Memories & Places</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks></NavLinks>
