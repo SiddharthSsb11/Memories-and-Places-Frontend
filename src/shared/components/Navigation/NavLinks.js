6612cf898890ade9149639ca4872025bdb7685ab
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-02-25 23:57:52 +0530

    navbar and inputs style edited

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -26,7 +26,7 @@ const NavLinks = props => {
       )}
       {!auth.isLoggedIn && (
         <li>
-          <NavLink to="/auth">AUTHENTICATE</NavLink>
+          <NavLink to="/auth">LOGIN</NavLink>
         </li>
       )}
       {auth.isLoggedIn && (
